9f496c7d6f09d2c3d1557df8c05e0faae4466b29
Jeffrey Varner <jdv27@cornell.edu>
2025-11-16 16:56:38 -0500

    Add week 13 lab materials for drug combination RL
    Added new lab notebook and supporting Julia files for CHEME-5660 L13b on drug combination design using reinforcement learning. Includes solution and student include files, project dependencies, and initial solution stubs. Also updated the week 12 notebook kernel display name.

---
+++ labs/week-13/L13b/solution/Types.jl
@@ -0,0 +1,10 @@
+abstract type MyAbstractContext end
+
+mutable struct MyExperimentalContext <: MyAbstractContext
+    
+    # initialize -
+    
+
+    # constructor -
+    MyExperimentalContext() = new(); # create new *empty* instance 
+end
